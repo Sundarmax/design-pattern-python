04f841453b70c60605c2d33374705cd7b4133651
sundarmax <sundar.info22@gmail.com>
2020-12-04 15:25:29 +0530

    inheritance update

---
+++ inheritance.py
@@ -0,0 +1,22 @@
+class Pet():
+
+    def __init__(self,name,species):
+        self.name = name
+        self.species = species
+
+    def __str__(self):
+        return "%s is a %s " % (self.name,self.species)
+
+
+class Dog(Pet):
+
+    def __init__(self,name,chase_cats):
+        '''Overloading example'''
+        super().__init__(name,"Dog")
+        self.chase_cats = chase_cats
+
+    def __str__(self):
+        additional_info = ""
+        if self.chase_cats:
+            additional_info = " Who chases cats"
+        return super().__str__() + additional_info
