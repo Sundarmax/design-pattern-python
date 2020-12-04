a476c37f8aca15a4dea686dd20662a5757b11734
sundarmax <sundar.info22@gmail.com>
2020-12-04 16:12:27 +0530

    Multiple inheritance update

--- inheritance.py
+++ inheritance.py
@@ -20,3 +20,27 @@ class Dog(Pet):
         if self.chase_cats:
             additional_info = " Who chases cats"
         return super().__str__() + additional_info
+
+class A(object):
+    def __init__(self):
+        print('A')
+
+    @staticmethod
+    def foo():
+        print('foo')
+
+class B(object):
+    def __init__(self):
+        print('B')
+
+    @staticmethod
+    def bar():
+        print('bar')
+
+# classes ordering 
+class C(B,A):
+    def foobar(self):
+        self.foo()
+        self.bar()
+
+
