2965f0680a4278be2592edc2a97f2ae4dc276cd5
sundarmax <sundar.info22@gmail.com>
2020-12-05 21:38:13 +0530

    singleton pattern update

---
+++ singleton.py
@@ -0,0 +1,19 @@
+class Singleton:
+
+    __ins = None
+
+    def __new__(cls,val=None):
+        if Singleton.__ins is None:
+            Singleton.__ins = object.__new__(cls)
+        Singleton.__ins.val = val
+        return Singleton.__ins
+
+# Test 
+a = Singleton(4)
+print(a.val)
+b = Singleton(5)
+print(b.val)
+print(a.val)
+# Singleton creates only one global object 
+# Check the id below is same for two objects.  
+print(id(a),id(b))
